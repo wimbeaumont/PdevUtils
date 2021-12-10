a8b09451c89b7a6d3cccdc9d3d62a58fb25791ca
Wim Beaumont <wim.beaumont@uantwerpen.be>
2021-12-10 14:23:51 +0100

    added flush in scpi_client_ser.cpp

--- scpi_client/src/scpi_client_ser.cpp
+++ scpi_client/src/scpi_client_ser.cpp
@@ -16,7 +16,7 @@
 
 
 #include <cstdio>
-//#include <errno.h>  
+#include <errno.h>  
 #include <fcntl.h>  // File Control Definitions for serial         
 #include <termios.h>// POSIX Terminal Control Definitions for serial 
 #include <cstdlib>     // atof 
@@ -25,7 +25,7 @@
 
 
 int serialportsetup() {
-#define SERPORT "/dev/ttyACM0"	
+#define SERPORT "/dev/ttyACM1"	
 int fd=open(SERPORT,O_RDWR | O_NOCTTY); //fd is locally defined but the open is static so also have to be closed
 
 	if(fd ==-1)
@@ -66,6 +66,26 @@ int main(int argc, char const *argv[]){
 //serial port 
 	int fd  = serialportsetup();
 	if ( fd == -1 ) exit(-1); 
+
+/*  break gives broken pipe and them MBED is stuck 
+// send break 
+	if ( tcsendbreak( fd,0)) {
+		printf( " break fails %s \n", strerror(errno)); 
+	} else {
+		printf("break sent \n");
+	}
+	
+ sleep(5);
+*/
+	
+// flush port 
+	if ( tcflush(fd, TCIOFLUSH)) {
+		printf( " flush fails %s \n", strerror(errno)); 
+	} else {
+		printf("flush done  \n");
+	}
+
+
 	printf("%s ver %s\n\r",argv[0], SCPICLIENSERVER );
 	int lc=0;
 
@@ -91,9 +111,9 @@ int main(int argc, char const *argv[]){
 		 printf("more messages defined than fits in array \n\r");
 	 	 nrmsg=NrCmd;
 	 }
-	 int ttcnt=0;
+	 int ttcnt=1;
 	 printf("init messages done \n\r");
-	 while (ttcnt < 50000) {
+	 while (ttcnt ) { // < 50000) {
 		lc=0;
 			while(lc< nrmsg) {
 				
@@ -105,14 +125,15 @@ int main(int argc, char const *argv[]){
 				buffer[valread]='\0';
 				int nrwr=write(fd,buffer,valread);	
 				buffer[valread-2]='\0';//suppress the newline in the output 
-				printf("msg nr %02d/%04d len %02d=%02d %-20s  ",lc,ttcnt++,valread,nrwr,buffer);
+				printf("msg nr %03d/%04d len %02d=%02d %-20s ",lc,ttcnt,valread,nrwr,buffer);
 				valread=read(fd, buffer , sizeof(buffer));//get full return message 
 				if( valread == 0) {
 					strcpy(buffer,"resp message is zerro");
 				} else { 		
 					buffer[valread-1]='\0';	// messages is not terminated with \0 !!! overwrite newline
 				}
-				printf(" resp %-*s len %02d \r\n",18,buffer, (int)strlen(buffer));				
+				buffer[(int)strlen(buffer)-1]='\0';  //supress the  \r 
+				printf(" resp nr %d %-*s len %03d %03d \r\n",ttcnt++,18,buffer,valread, (int)strlen(buffer));				
 				buffer[0]='\0';
 				usleep(10000);
                 lc++;
